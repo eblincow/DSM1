71a4931ba4748155946212ee27552e4b26f739f7
Eric Blincow <eblincow@barracuda.com>
2016-09-12 23:36:45 -0400

    Yay uppers

--- dsm.erl
+++ dsm.erl
@@ -1,6 +1,11 @@
 -module(dsm).
--export([dsm/0,dsm2/1]).
+-export([dsm/0,dsm2/1,add/2]).
 
     dsm() -> io:fwrite("Eric's DSM Quiz\n").
-    dsm2(X) -> io:fread("Good morning, JustJoking!","s").
+    
+    dsm2(X) when X>25 -> io:fread("Good morning, JustJoking!","s");
+    dsm2(D) when D>0 -> io:fwrite("Wow, you got 1 to 25!");    
+    dsm2(0) -> io:fwrite("Wow, you got a zero!"). 
+    
+add(A,B) -> A+B.
 
